34bf42ba8441715f34b485ebc396daffac3aa7a7
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2024-05-15 11:01:22 +0000

    binary search tree add understood

--- specs/bst/bst.test.js
+++ specs/bst/bst.test.js
@@ -17,22 +17,53 @@ right - Node/object - the right node which itself may be another tree
 */
 
 class Tree {
-  // code goes here
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      let currentNode = this.root;
+      while (currentNode) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = new Node(value);
+            console.log(this.root);
+            return;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = new Node(value);
+            console.log(this.root);
+            return;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
 }
 
 // you might consider using a Node class too
-// class Node {
-//   // code maybe goes here
-// }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 // unit tests
 // do not modify the below code
-describe.skip("Binary Search Tree", function () {
+describe("Binary Search Tree", function () {
   it("creates a correct tree", () => {
     const nums = [3, 7, 4, 6, 5, 1, 10, 2, 9, 8];
     const tree = new Tree();
     nums.map((num) => tree.add(num));
-    const objs = tree.toObject();
+    const objs = tree.root;
     // render(objs, nums);
 
     expect(objs.value).toEqual(3);
